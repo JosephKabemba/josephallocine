cae0e1e31fe8d2669788babc33dc521c8d7154b0
Joseph Kabemba <josephkabemba77@gmail.com>
2020-08-09 01:08:21 +0100

    Premier commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Card, Input, Button } from "semantic-ui-react";
+import "./App.css";
+import Film from "./components/Film";
+import axios from "axios";
 
 function App() {
+  const [films, setFilms] = useState([]);
+  const [filmRecherche, setFilmRecherche] = useState("Anne");
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=963b7435377e0921bfa89573f3501e4a&query=${filmRecherche}`
+      )
+      .then((res) => setFilms(res.data.results))
+      .catch((err) => console.log(err));
+  }, [filmRecherche]);
+
+  // useEffect(() => {});
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Allocine Clone</h1>
+      <Input
+        placeholder="Rechercher un film"
+        onChange={(e) => setFilmRecherche(e.target.value)}
+      />
+      <Button content="Rechercher" />
+      <h2>{filmRecherche}</h2>
+      <Card.Group id="film-container">
+        {films.map((film, i) => {
+          return (
+            <Film
+              key={i}
+              titre={film.title}
+              poster={`https://image.tmdb.org/t/p/original${film.poster_path}`}
+              description={film.overview.slice(0, 15).concat("...")}
+              dateSortie={film.release_date}
+            />
+          );
+        })}
+      </Card.Group>
     </div>
   );
 }
